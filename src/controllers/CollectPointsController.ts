43d24701077349c449f4ffc34a1ecf3a5e7a8757
lucascs20182 <lucascs20182@gmail.com>
2020-10-26 11:54:07 -0300

    data validation

--- src/controllers/CollectPointsController.ts
+++ src/controllers/CollectPointsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm'; //repository pattern
 import CollectPoints from '../models/CollectPoint';
 import collectPointsView from '../views/collect_points_view';
+import * as Yup from 'yup';
 
 export default {
     async index(request: Request, response: Response) {
@@ -38,11 +39,29 @@ export default {
         const images = requestImages.map(image => {
             return { path: image.filename }
         })
-    
-        const collectPoint = collectPointsRepository.create({
+
+        const data = {
             name, latitude, longitude, about, instructions,
             opening_hours, open_on_weekends, images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         });
+
+        await schema.validate(data, { abortEarly: false });
+    
+        const collectPoint = collectPointsRepository.create(data);
     
         await collectPointsRepository.save(collectPoint);
     
